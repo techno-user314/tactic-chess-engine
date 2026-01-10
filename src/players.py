2d1e676352da29c1d772f959b098fa4dd8afcb0b
techno-user314 <zharwood314@gmail.com>
2026-01-09 17:38:14 -0700

    Add control button functionality to Players

--- src/players.py
+++ src/players.py
@@ -11,10 +11,12 @@ PIECE_VALUES = {chess.PAWN:1,
 BOT_DEPTH_PLY = 2
 
 class Player:
-    def __init__(self, is_white, live_board, logger):
+    def __init__(self, is_white, live_board, controls, logger):
         self.color = is_white
         self.play_board = live_board
         self.log = logger
+
+        controls[1].bind("<Button-1>", self.undo_ply, add="+")
 
         if self.color: self.log("White player created\n")
         else: self.log("Black player created\n")
@@ -27,9 +29,14 @@ class Player:
             return True
         return False
 
+    def undo_ply(self, event):
+        if self.color and self.play_board.ply() > 0:
+            self.play_board.pop()
+            self.play_board.render()
+
 class HumanPlayer(Player):
-    def __init__(self, is_white, live_board, analysis_board, logger):
-        super().__init__(is_white, live_board, logger)
+    def __init__(self, is_white, live_board, analysis_board, controls, logger):
+        super().__init__(is_white, live_board, controls, logger)
         self.play_board.surface.bind("<Button-1>", self.on_click, add="+")
 
     def on_click(self, event):
@@ -53,19 +60,24 @@ class HumanPlayer(Player):
                 self.play_board.render()
 
 class Bot(Player):
-    def __init__(self, is_white, live_board, analysis_board, logger):
-        super().__init__(is_white, live_board, logger)
-        self.play_board.surface.bind("<Button-1>", self.on_click, add="+")
+    def __init__(self, is_white, live_board, analysis_board, controls, logger):
+        super().__init__(is_white, live_board, controls, logger)
+        trigger_button = 2 - int(self.color) * 2
+        controls[trigger_button].bind("<Button-1>", self.bot_trigger, add="+")
+        controls[trigger_button].bind("<Button-1>", self.bot_trigger, add="+")
 
         self.move_scores = {}
 
+        self._search_calls = 0
+
     # --- Event response ---
-    def on_click(self, event):
+    def bot_trigger(self, event):
         if self.is_my_turn():
             self.play_best_move()
 
     # --- Finding and playing a move ---
     def get_move_score(self, move, board, depth):
+        self._search_calls += 1
         board.push(move)
         if depth == 0 or board.is_game_over():
             best = self.evaluate_pos(board)
@@ -81,6 +93,7 @@ class Bot(Player):
     def play_best_move(self):
         self.move_scores = {}
         test_board = chess.Board(self.play_board.fen())
+        self._search_calls = 0
         for move in self.play_board.legal_moves:
             score = self.get_move_score(move, test_board, BOT_DEPTH_PLY)
             self.move_scores.update({move:score})
@@ -88,7 +101,11 @@ class Bot(Player):
         all_good_moves = [key for key, value in self.move_scores.items()
                           if value == best_move_score]
         best_move = pick_from(all_good_moves)
-
+        self.log(f"{"White" if self.color else "Black"} bot"
+                 + f" picked {self.play_board.san(best_move)} randomly"
+                 + f" out of {len(all_good_moves)} considered moves."
+                 + f"\n    - It took {self._search_calls} recursions"
+                 + f" at depth {BOT_DEPTH_PLY}.\n")
         self.play_board.play_move(best_move)
 
     # --- Helper functions ---
