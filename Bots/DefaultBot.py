52bc29a69bbd7fc277f18b9756c6d28e94fef73a
techno-user314 <zharwood314@gmail.com>
2026-01-29 15:05:59 -0700

    Use custom Board's  built-in material count

--- Bots/DefaultBot.py
+++ Bots/DefaultBot.py
@@ -2,6 +2,5 @@ from src.players import Bot
 
 class DefaultBot(Bot):
     def evaluate_pos(self, board):
-        my_mat = self.material_count(board)
-        other_mat = self.material_count(board, for_opponent=True)
-        return my_mat - other_mat
+        mat = board.material_count()
+        return mat[self.color] - mat[not self.color]
